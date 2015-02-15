9d90318608129a12e48815d336cc5f4132c84809
Mike Dews <mdews@dewser.com>
2015-02-15 17:09:19 -0500

    Powershell Empty Folder cleanup
    Used this after cleaning up a music library.  It does not appear clean
    up all the empty folders, it could be due to hidden files preventing the
    deletion, but it does throw an error.   It does clean up a good majority
    of them.
    
    Add -whatif to line 23 if you want to see what it will remove.  You can
    also take the $emptydirs value and pipe it to an export-csv to see the
    files you will be removing.

---
+++ Remove-EmptyDirs.ps1
@@ -0,0 +1,25 @@
+﻿<#
+Remove-EmptyDirs.ps1
+Created 2/15/2015
+By Mike Dews
+
+Clean up empty folders and subfolders
+Got the base of the script from: 
+http://blogs.technet.com/b/heyscriptingguy/archive/2014/04/05/weekend-scripter-organize-music-collection-with-powershell.aspx
+Problem was that it was throwing a null path error at the remove operation.
+Decided to send the data to loop so each one goes through individually.
+
+#>
+
+Param(
+    [string]$SeachDir
+)
+
+$emptydirs = Get-Childitem $SeachDir –Recurse –Directory | `
+where { $_.getdirectories().count –eq 0 –and $_.getfiles().count –eq 0 }
+
+foreach($emptydir in $emptydirs)
+{
+    Remove-Item -Path $emptydir.FullName
+
+}
